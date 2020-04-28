e6c7ecb985b45893a2438585551846fcfaf7c794
Md. Masudur Rahman <masudurhimel@gmail.com>
2020-04-29 02:21:44 +0600

    MTV added

--- first_app/views.py
+++ first_app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Webpage, Topic, AccessDate
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World !!!")
+    webpg = AccessDate.objects.order_by('date')
+    webpageDict = {'access_record': webpg} 
+    return render(request, "index.html", context = webpageDict)
 
 def appPage(request):
     return HttpResponse("This is the firstApp page !!!")
 
 def templatePage(request):
     x = {'insert_me' : "This is from views.py"}
-    return render(request, "first_app/index.html", context= x)
+    return render(request, "first_app/templatePage.html", context= x)
 
 def imagePage(request):
     return render(request, "first_app/imagePageHtml.html")
