a91c6c6db9b1b8364a3593c88b67e179d2799c5c
Md. Masudur Rahman <masudurhimel@gmail.com>
2020-05-06 01:58:56 +0600

    User form added

--- User_App/admin.py
+++ User_App/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from User_App.models import UserProfileInfo
+from django.contrib.auth.models import User
 
 # Register your models here.
+admin.site.register(UserProfileInfo)
